f55e65fad4ea521abecaa1aac6d86eb8f7b7ea6d
tvs <venkatasandeept@gmail.com>
2020-10-06 23:49:04 +0530

    conditional operators

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,20 +1,33 @@
 answer = 5
 #print("Please Guess number between 1 and 10: ")
 guess = int(input("Please Guess number between 1 and 10: "))
-
-if guess < answer:
-	print("Please guess higher")
-	guess=int(input())
-	if guess==answer:
-		print("welldone you have guessed correctly")
-	else:
-		print("sorry, you are wrong")
-elif guess > answer:
-	print("Please guess lower")
-	guess=int(input())
-	if guess==answer:
-		print("welldone you have guessed correctly")
-	else:
-		print("sorry, you are wrong")
+if guess == answer:
+    print("you got it first time")
 else:
-	print("Guessed correctly")
+    if guess < answer:
+        print("please guess higher")
+    else:
+        print("please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("well done")
+    else:
+        print("sorry you are wrong")
+
+
+#if guess < answer:
+#	print("Please guess higher")
+#	guess=int(input())
+#	if guess==answer:
+#		print("welldone you have guessed correctly")
+#	else:
+#		print("sorry, you are wrong")
+#elif guess > answer:
+#	print("Please guess lower")
+#	guess=int(input())
+#	if guess==answer:
+#		print("welldone you have guessed correctly")
+#	else:
+#		print("sorry, you are wrong")
+#else:
+#	print("Guessed correctly")

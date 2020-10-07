50cabc207111534701e4aedc740bd597b0e0fd3e
tvs <venkatasandeept@gmail.com>
2020-10-07 23:28:32 +0530

    boolean expressions ,In and Not in

---
+++ InAndNotIn.py
@@ -0,0 +1,14 @@
+parrot = "norwegian Blue"
+
+letter = input("please enter a character: ")
+
+if letter in parrot:
+    print("{} is in {}".format(letter,parrot))
+else:
+    print("i dont need that character")
+
+
+activity = input("what to do in today: ")
+
+if "cinema" not in activity:
+    print("i want to go cinema")
---
+++ booleanExpressions.py
@@ -0,0 +1,18 @@
+#day = "thursday"
+#temperature = 30
+#raining = False
+#if (day == "saturday" and temperature > 27)  or not raining:
+#     print("go for swmming")
+#else:
+#    print("learn python")
+
+if 0:
+    print("true")
+else:
+    print("false")
+name = input("please enter your name: ")
+#if name:
+if name != "":
+    print("hello " + name)
+else:
+    print("enter the name again")
